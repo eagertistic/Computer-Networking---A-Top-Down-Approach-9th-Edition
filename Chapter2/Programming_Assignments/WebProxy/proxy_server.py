eaddb1b4c0b0188939718e06f7deb6dec4942b2d
eagertistic <pengjiaqi2@gmail.com>
2026-04-24 20:48:37 +0100

    implement post method

--- Chapter2/Programming_Assignments/WebProxy/proxy_server.py
+++ Chapter2/Programming_Assignments/WebProxy/proxy_server.py
@@ -18,29 +18,62 @@ while 1:
         print('Ready to serve...')
         print('Received a connection from:', addr)
 
-        message = tcpCliSock.recv(1024).decode("utf-8", errors="ignore")
+        message = tcpCliSock.recv(2048).decode("utf-8", errors="ignore")
         print(message)
 
-        lines = message.splitlines()
+        headers_part, body = message.split("\r\n\r\n", 1)
+
+        lines = headers_part.splitlines()
         request_line = lines[0]
         method, target_url, version = request_line.split()
+
         hostname = ""
+        content_type = ""
 
         for line in lines:
-            if line.startswith("Host:"):
+            if line.lower().startswith("host:"):
                 hostname = line.split(":", 1)[1].strip()
+                break
+        for line in lines:
+            if line.lower().startswith("content-type:"):
+                content_type = line + "\r\n"
                 break
 
         hostname = hostname.split(":", 1)[0]
 
-        if method != "GET":
+        accept_method = ("GET", "POST")
+        if method not in accept_method:
             tcpCliSock.sendall(b"HTTP/1.0 501 Not Implemented\r\n\r\n")
             continue
 
         parsed_url = urlparse(target_url)
-        # extract path /index.html
+        # extract path for example: /index.html
         path = parsed_url.path or "/"
         filename = path.split("/")[-1] or "index.html"
+
+        # client sends a POST request to the orignal server
+        if method == "POST":
+            c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            c.connect((hostname, 80))
+
+            request_body = body.encode("utf-8")
+
+            # updates the request file and sends a response message
+            request_headers = (f"POST {path} HTTP/1.0\r\n"
+                               f"Host: {hostname}\r\n"
+                               f"{content_type}"
+                               f"Content-Length: {len(request_body)}\r\n"
+                               f"\r\n").encode("utf-8")
+
+            c.sendall(request_headers + request_body)
+            response = b""
+            while True:
+                data = c.recv(2048)
+                if not data:
+                    break
+                response += data
+            tcpCliSock.sendall(response)
+            continue
 
         try:
             # Check wether the file exist in the cache
